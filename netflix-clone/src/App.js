e3963f1a33992d7ec55d4bd4a37cabf7b8609001
prabal7 <40392568+prabal7@users.noreply.github.com>
2021-07-07 13:22:41 +0530

    add: rows

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import Row from './Row'
-import requests from './request';
+import logo from "./logo.svg";
+import "./App.css";
+import Row from "./Row";
+import requests from "./request";
 
 function App() {
   return (
     <div className="App">
-      <Row title="Netflix Originals" fetchURL={requests.fetchNetflixOriginals}/>
+      <Row
+        title="Netflix Originals"
+        fetchURL={requests.fetchNetflixOriginals}
+      />
       <Row title="Trending Now" fetchURL={requests.fetchTrending} />
-
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romance" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 }
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,24 +1,38 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import "./row.css";
 
-function Row({ title,fetchURL }) {
+const base_url = "https://image.tmdb.org/t/p/original/";
+function Row({ title, fetchURL }) {
   const [movies, setMovies] = useState([]);
 
-  useEffect(()=>{
-    async  function fetchData(){
-     const request= await axios.get(fetchURL);
-     console.log(request.data.results);
-     setMovies(request.data.results)
-     return request;
-      }
-      fetchData();
-  },[fetchURL])
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      setMovies(request.data.results);
 
+      return request;
+    }
+    fetchData();
+  }, [fetchURL]);
+  console.log(movies);
 
-  
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
+
+      <div className="row_posters">
+        {movies.map((movie) => {
+          return (
+            <img
+              key={movie.id}
+              className="row_poster"
+              src={`${base_url}${movie.poster_path}`}
+              alt={movie.name}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
