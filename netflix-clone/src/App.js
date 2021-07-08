cf040faf0b7ae4bec0581470d0bf9f055f93cefc
prabal7 <40392568+prabal7@users.noreply.github.com>
2021-07-08 12:24:14 +0530

    add: banner

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -2,13 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import Row from "./Row";
 import requests from "./request";
-
+import Banner from "./Banner"
 function App() {
   return (
     <div className="App">
+
+    <Banner />
       <Row
         title="Netflix Originals"
         fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow
       />
       <Row title="Trending Now" fetchURL={requests.fetchTrending} />
       <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
